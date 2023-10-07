8722ace8eda03ef8894fa0779912966ceee20a9d
ceGarcia23 <cjgarcia2346@gmail.com>
2023-10-06 23:43:32 -0500

    add branch hotfix/stylos/bootstrap/cita

--- tasks/views.py
+++ tasks/views.py
@@ -3,10 +3,11 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
-from.forms import TaskForm
+from .forms import TaskForm
 from .models import Task
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def home(request):
@@ -38,28 +39,25 @@ def signup(request):
             {"form": UserCreationForm, "error": "Password do not match"},
         )
 
+
 @login_required()
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
-    return render(request, "tasks.html", {
-        "tasks": tasks
-    })
+    return render(request, "tasks.html", {"tasks": tasks})
 
 
 @login_required()
 def tasks_completed(request):
-    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
-    return render(request, "tasks.html", {
-        "tasks": tasks
-    })
+    tasks = Task.objects.filter(
+        user=request.user, datecompleted__isnull=False
+    ).order_by("-datecompleted")
+    return render(request, "tasks.html", {"tasks": tasks})
 
 
 @login_required()
 def create_task(request):
     if request.method == "GET":
-        return render(request, "create_task.html", {
-            "form": TaskForm
-        })
+        return render(request, "create_task.html", {"form": TaskForm})
     else:
         try:
             form = TaskForm(request.POST)
@@ -68,10 +66,11 @@ def create_task(request):
             new_task.save()
             return redirect("tasks")
         except ValueError:
-            return render(request, "create_task.html", {
-            "form": TaskForm,
-            "error": "Please provide valida data"
-            })
+            return render(
+                request,
+                "create_task.html",
+                {"form": TaskForm, "error": "Please provide valida data"},
+            )
 
 
 @login_required()
@@ -79,22 +78,26 @@ def task_detail(request, task_id):
     if request.method == "GET":
         task = get_object_or_404(Task, pk=task_id, user=request.user)
         form = TaskForm(instance=task)
-        return render(request, "task_detail.html", {
-            "task": task,
-            "form": form,
-        })
-    else:
-        try:
-            task = get_object_or_404(Task, pk=task_id,  user=request.user)
-            form = TaskForm(request.POST, instance=task)
-            form.save()
-            return redirect('tasks')
-        except ValueError:
-            return render(request, "task_detail.html", {
+        return render(
+            request,
+            "task_detail.html",
+            {
                 "task": task,
                 "form": form,
-                "error": "Error Updating Task"
-            })
+            },
+        )
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect("tasks")
+        except ValueError:
+            return render(
+                request,
+                "task_detail.html",
+                {"task": task, "form": form, "error": "Error Updating Task"},
+            )
 
 
 @login_required()
